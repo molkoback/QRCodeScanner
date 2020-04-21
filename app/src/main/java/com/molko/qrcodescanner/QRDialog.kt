b63a60e1c962f9aa794ef0e04a0bbf882c694dc7
molko <molkoback@gmail.com>
2020-04-21 11:20:49 +0300

    Clipboard

--- app/src/main/java/com/molko/qrcodescanner/QRDialog.kt
+++ app/src/main/java/com/molko/qrcodescanner/QRDialog.kt
@@ -1,16 +1,23 @@
 package com.molko.qrcodescanner
 
+import android.R.attr.label
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.content.Context
 import android.content.DialogInterface.OnDismissListener
 import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.qrdialog.view.*
 
-private fun copy(text: String) {
-    // TODO
+private fun copy(context: Context, text: String) {
+    val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+    val clip = ClipData.newPlainText(context.getString(R.string.app_name), text)
+    clipboard.setPrimaryClip(clip)
+    Toast.makeText(context, context.getString(R.string.copy_done), Toast.LENGTH_SHORT).show()
 }
 
-private fun open(text: String) {
+private fun open(context: Context, text: String) {
     // TODO
 }
 
@@ -19,10 +26,10 @@ fun showQRDialog(context: Context, text: String, listener: OnDismissListener? = 
     val view = inflater.inflate(R.layout.qrdialog, null)
     view.textQR.text = text
     view.buttonCopy.setOnClickListener {
-        copy(text)
+        copy(context, text)
     }
     view.buttonOpen.setOnClickListener {
-        open(text)
+        open(context, text)
     }
     
     val builder = AlertDialog.Builder(context)
